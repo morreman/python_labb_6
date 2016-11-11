7949d11960f74370f880faed1af37e920cb61761
Marten <persson_dunk@msn.com>
2016-11-11 13:02:57 +0100

    finished

--- assignment_6.py
+++ assignment_6.py
@@ -20,7 +20,8 @@ def index():
         return render_template("index.html")
 
 
-# using post method to post information from the form
+# using post method to post information from the form.
+# using get method to display all recipes.
 @app.route('/browse', methods=['POST', 'GET'])
 def browse():
     if request.method == 'GET':
@@ -39,23 +40,22 @@ def browse():
         img = request.form['img']
         comment = request.form['comment']
         add_recipe(recipe_name, ingridient_1,ingridient_2,ingridient_3,img,comment) # send all of the info to add_user method.
-        #complete_recipe = [recipe_name,ingridient_1,ingridient_2,ingridient_3,img,comment]
         return render_template("index.html")
 
 
 
-#open the json file find the right array to add the new user to. (in this case the only array available).
+#Appends a new recipe to the json database.
 def add_recipe(recipe_name,ingridient_1 , ingridient_2, ingridient_3, img, comment):
     data = {"recipe_name":recipe_name, "ingridient_1":ingridient_1, "ingridient_2":ingridient_2, "ingridient_3":ingridient_3, "img":img, "comment":comment}
     with open('database.json') as json_file:
         recipe = json.load(json_file)
         recipe["recipes"].append(data)
     with open("database.json", "w") as json_file:
-        json.dump(recipe, json_file, indent=3) # Indent for visual aid.
+        json.dump(recipe, json_file, indent=4) # Indent for visual aid.
     pass
 
 
-
+#Used for searching recipes.
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'GET':
@@ -67,15 +67,16 @@ def search():
                     return render_template("recipe.html", a_recipe = a_recipe)
 
 
+#Route for edit page.
 @app.route('/edit')
 def edit():
     return render_template("edit.html")
 
-
+#Route for editing recipies.
 @app.route('/update_json/<string:recipe_id>', methods = ['POST'])
 def update_json(recipe_id):
     if request.method == 'POST':
-        with open('database.json', 'r+') as f:
+        with open('database.json', 'r+') as f:      # Open file r+ for read and append.
             data = json.load(f)
             recipes = data["recipes"]
             for recipe in recipes:
@@ -86,11 +87,13 @@ def update_json(recipe_id):
                     recipe['ingridient_3'] = request.form['thirdIngridient']
                     recipe['img'] = request.form['img']
                     recipe['comment'] = request.form['comment']
-                    f.seek(0)
-                    json.dump(data, f, indent=3)
+                    f.seek(0)                                       #seek to beginning of document.
+                    f.write(json.dumps(data, indent=4))             # write the data to jsonfile with indention for visual aid.
+                    f.truncate()
                     return render_template("edit.html")
 
 
+#Api route to see a json recipe in webbrowser.
 @app.route('/api/browse/<recipe>') #route a user
 def user(recipe):
     with open('database.json') as json_file:
@@ -100,7 +103,7 @@ def user(recipe):
             if a_recipe["recipe_name"] == recipe:
                 return jsonify(a_recipe)
 
-
+#Api route to see all json recipes in webbrowser.
 @app.route('/api/recipes_all')
 def recipes_all():
     with open('database.json') as json_file:
@@ -108,6 +111,7 @@ def recipes_all():
         return jsonify(recipes)
 
 
+# route all pages that doesnt exist here for error handling. 
 @app.errorhandler(404) #route 404 page
 def page_not_found(e):
     return render_template('404.html'), 404
